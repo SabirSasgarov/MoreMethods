eb320f28f1f7bd9e0b597c16be54773c04b98af2
Sabir A. Asgarov <sabirsas@code.local>
2026-02-05 17:44:01 +0400

    Code Start

---
+++ SabirMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SabirMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "salam sagol";
+
+            //Console.WriteLine(word.Remove(4));
+            //Console.WriteLine(word.Substring(0,4));
+            //Console.WriteLine(word.Trim());
+            //Console.WriteLine(word.IndexOf(' '));
+            //string[] words = word.Split(' ');
+            //Console.WriteLine(string.Concat(words));
+            //Console.WriteLine(word.Replace('',' '));
+
+            int[] numbers= [ 1, 2, -3, 4, -5, 6, -7 ];
+            MusbeteCevir(ref numbers);
+
+
+
+        }
+        public static int[] MusbeteCevir(ref int[] nums)
+        {
+
+            return new int[nums.Length];
+        }
+    }
+}
